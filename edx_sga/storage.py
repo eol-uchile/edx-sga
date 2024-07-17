816906a8a99e6ed340978ecc0541b420aef7d630
fsologureng <fsologur@uchile.cl>
2024-07-17 12:16:18 -0400

    fix: removes error in compile time bc undefined value

--- edx_sga/storage.py
+++ edx_sga/storage.py
@@ -3,4 +3,4 @@ from django.conf import settings
 
 
 class SgaStorage(S3Boto3Storage):
-    bucket_name = settings.SGA_STORAGE_BUCKET_NAME
+    bucket_name = vars(settings).get('SGA_STORAGE_BUCKET_NAME', settings.FILE_UPLOAD_STORAGE_BUCKET_NAME)
